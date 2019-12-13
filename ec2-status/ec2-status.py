b1ba1ebfb301263497ae2e79eb7feee956054e33
shawn2506 <dsouzashawn62@yahoo.in>
2019-12-13 15:30:03 +0530

    Dynamic ec2-status script

---
+++ ec2-status/ec2-status.py
@@ -0,0 +1,10 @@
+#!/usr/local/env python
+
+import boto3
+session = boto3.session.Session(profile_name = "shawn")
+ec2_res = session.resource(service_name="ec2", region_name="us-east-1")
+
+#print dir(ec2_res)
+instance_id = raw_input("Enter your instance-id ==> ")
+my_instance = ec2_res.Instance(id = instance_id)
+print(my_instance.state['Name'])
